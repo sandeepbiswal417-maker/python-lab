a33eabc45ce1e2a124a4094b4486a7e46e934b38
sandeepbiswal417-maker <sandeepbiswal417@gmail.com>
2026-04-09 21:16:07 +0530

    Add files via upload

---
+++ program for number comparison.py
@@ -0,0 +1,9 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater")
+elif a < b:
+    print("Second number is greater")
+else:
+    print("Both numbers are equal")
